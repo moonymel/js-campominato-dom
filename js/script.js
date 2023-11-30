df40296f8fcc0ff008d85e50568414a7483d721c
moonymel <musumeci.me@gmail.com>
2023-11-30 14:47:20 +0100

    update (esercizio corretto)

--- js/script.js
+++ js/script.js
@@ -10,36 +10,42 @@ function createCell(num) {
     return element;
 }
 
-// VARIABILE DEL CONTENUTO DELLA GRIGLIA
+// CREO LA FUNZIONE PER INIZIARE IL GIOCO
 
-let grid = document.getElementById('grid');
+function startToPlay (){ 
+    // VARIABILE DEL CONTENUTO DELLA GRIGLIA
+    let grid = document.getElementById('grid');
+    
+    // VARIABILE DEL BOTTONE
+    let button = document.querySelector('.btn');
 
-// VARIABILE DEL BOTTONE
-
-let button = document.querySelector('.btn');
-
-// CREO EVENTO PER IL CLICK SUL BOTTONE
-
-button.addEventListener('click', function(){
-
-    grid.innerHTML = '';
-
-    // AVVIO CICLO FOR PER IL NUMERO DI CASELLE
-    for(let i=0; i<100; i++) { 
-        
-        // CHIAMO LA FUNZIONE PER LA CELLA
-        let cell = createCell(i+1);
-        
-        // BONUS
-        
-        cell.addEventListener('click', function(){
-            this.classList.add('new-bg');
-            console.log(`Questa è la cella numero ${i+1}`)
+    // CREO EVENTO PER IL CLICK SUL BOTTONE   
+    button.addEventListener('click', function(){
+    
+        grid.innerHTML = '';
+    
+        // AVVIO CICLO FOR PER IL NUMERO DI CASELLE
+        for(let i=0; i<100; i++) { 
             
-        })
-        
-         // AGGIUNGO LA CELLA NEL DOM
-        grid.appendChild(cell);
-        
-    }
-})
+            // CHIAMO LA FUNZIONE PER LA CELLA
+            let cell = createCell(i+1);
+            
+            // BONUS 
+            cell.addEventListener('click', function(){
+                this.classList.add('new-bg');
+                console.log(`Questa è la cella numero ${i+1}`)
+                
+            })
+            
+             // AGGIUNGO LA CELLA NEL DOM
+            grid.appendChild(cell);
+            
+        }
+    })
+}
+
+
+startToPlay();
+
+
+
